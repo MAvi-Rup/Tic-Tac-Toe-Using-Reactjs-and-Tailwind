a0568cc297af2b94de325eb1a679d9a622d875ef
MAvi-Rup <avirupmondal19@gmail.comm>
2024-06-11 02:47:00 +0600

    Tic Tac Toe Game building Started

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import GameBoard from "./GameBoard";
 
 const Game = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <div className="flex justify-center">
       <div>
-        <GameBoard />
+        <GameBoard value={squares} />
       </div>
       <div></div>
     </div>
